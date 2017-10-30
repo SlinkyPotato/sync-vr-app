ea572555b833610679f49b218441d8fcc04fc686
Brian Patino <patinobrian@gmail.com>
2017-10-30 14:39:45 -0400

    add scoreboard

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
+    public Text scoreText;
     
     private Rigidbody rb;
+    private int score;
 
     // Initialization code goes here
     void Start() {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        setScoreText();
     }
 
     // Called before any physiscs calculations is called
@@ -21,5 +26,17 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         
         rb.AddForce(movement * speed);
+    }
+    
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+            score++;
+            setScoreText();
+        }
+    }
+
+    void setScoreText() {
+        scoreText.text = "Score: " + score.ToString();        
     }
 }
