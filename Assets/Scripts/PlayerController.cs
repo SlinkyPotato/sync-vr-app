4298a024b18b7f6bb9e40021918439af8f51febd
Brian Patino <patinobrian@gmail.com>
2017-10-30 00:13:05 -0400

    add player controller script and support for rider ide

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Console.Write("Hello world!");
+	}
+
+    // Called before any physiscs calculations is called
+    private void FixedUpdate() {
+	    Console.Write("Hello world!");
+    }
+}
