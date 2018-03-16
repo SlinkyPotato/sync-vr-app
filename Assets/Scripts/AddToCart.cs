fcac1ea7b90a81b748b810d73f76ab3caf5d6203
kag359six <kristopherguz@gmail.com>
2018-03-16 19:20:35 -0400

    The spring break demo for Synchrony

---
+++ Assets/Scripts/AddToCart.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AddToCart : MonoBehaviour {
+
+    private Cart cart;
+    private UserAvatar avatar;
+
+	// Use this for initialization
+	void Start () {
+        cart = GameObject.Find("GameManager").GetComponent<Cart>();
+        avatar = GameObject.FindWithTag("Avatar").GetComponent<UserAvatar>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPurchasableClick()
+    {
+        GameObject textObj = EventSystem.current.currentSelectedGameObject;
+        Text itemText = textObj.GetComponent<Text>();
+        if(itemText.text.Equals("Remove From Cart"))
+        {
+            
+            Purchasable item = itemText.GetComponent<Purchasable>();
+            cart.remove(item);
+            GameObject limb = item.itemType == Purchasable.ItemType.top ? avatar.top : avatar.bottom;
+            SkinnedMeshRenderer avatarRenderer = limb.GetComponent<SkinnedMeshRenderer>();
+            avatarRenderer.material = null;
+            itemText.text = "Add To Cart";
+        } else
+        {
+
+            Purchasable item = itemText.GetComponent<Purchasable>();
+            cart.removeSimilarItems(item.itemType);
+            cart.add(item);
+            GameObject limb = item.itemType == Purchasable.ItemType.top ? avatar.top : avatar.bottom;
+            SkinnedMeshRenderer avatarRenderer = limb.GetComponent<SkinnedMeshRenderer>();
+            avatarRenderer.material = Resources.Load("Materials/" + item.materialName) as Material;
+            resetItemText(item.itemType);
+            itemText.text = "Remove From Cart";
+        }
+        
+    }
+
+    private void resetItemText(Purchasable.ItemType type)
+    {
+        GameObject[] labels = GameObject.FindGameObjectsWithTag("AddToCartLabel");
+        foreach(GameObject g in labels) {
+            if(type == g.GetComponent<Purchasable>().itemType) //reset text of items in same category as one being added to cart
+            {
+                Text t = g.GetComponent<Text>();
+                t.text = "Add To Cart";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Purchasable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Purchasable : MonoBehaviour {
+
+    public enum ItemType {top, bottom, shoes};
+    public string itemName;
+    public string itemPrice;
+    public ItemType itemType;
+    public string materialName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
