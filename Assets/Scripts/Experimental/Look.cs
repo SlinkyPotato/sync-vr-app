c2d65cad0849905ade7c9bf2621b2e7d5ce06fc4
kag359six <kristopherguz@gmail.com>
2018-04-01 16:10:05 -0400

    room remodel, portal device, synchrony logo, lighting changes.

--- Assets/Scripts/Experimental/Look.cs
+++ Assets/Scripts/Experimental/Look.cs
@@ -11,12 +11,12 @@ public class Look : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		rx = transform.eulerAngles.x;
+		ry = transform.eulerAngles.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Cursor.lockState = CursorLockMode.Locked;
 		ry += sensitivity * Input.GetAxis ("Mouse X");
 		rx += sensitivity * Input.GetAxis ("Mouse Y");
 		rx = Mathf.Clamp (rx, -35, 50);
---
+++ Assets/Scripts/Experimental/PortalCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+
+	public Transform portalSource;
+	public Transform portalDest;
+	private Transform mainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+		mainCamera = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		Vector3 offset = mainCamera.position - portalSource.position;
+		transform.position = portalDest.position + (offset);
+
+		transform.rotation = portalDest.rotation * mainCamera.rotation;
+		float angleY = Mathf.Clamp (transform.eulerAngles.y, 80, 100);
+		Quaternion clampedRotation = Quaternion.Euler (transform.eulerAngles.x, angleY,
+			transform.eulerAngles.z);
+		transform.rotation = clampedRotation;
+	}
+}
