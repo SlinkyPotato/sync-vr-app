7a5178535ddb886f7f7e390b5c0be7419c76caf8
kag359six <kristopherguz@gmail.com>
2018-04-04 15:48:19 -0400

    some changes to the room + attempts to fix skinned mesh renderer issues

---
+++ Assets/Scripts/Debugging/BoneCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneCheck : MonoBehaviour {
+
+	public SkinnedMeshRenderer targetMeshRenderer;
+
+	// Use this for initialization
+	void Start () {
+		Dictionary<string, Transform> boneMap = new Dictionary<string, Transform>();
+		foreach (Transform bone in targetMeshRenderer.bones)
+			boneMap[bone.gameObject.name] = bone;
+
+
+		SkinnedMeshRenderer myRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+
+		Transform[] newBones = new Transform[myRenderer.bones.Length];
+		for (int i = 0; i < myRenderer.bones.Length; ++i)
+		{
+			GameObject bone = myRenderer.bones[i].gameObject;
+			if (!boneMap.TryGetValue(bone.name, out newBones[i]))
+			{
+				Debug.Log("Unable to map bone \"" + bone.name + "\" to target skeleton.");
+				break;
+			}
+		}
+		myRenderer.bones = newBones;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+}
